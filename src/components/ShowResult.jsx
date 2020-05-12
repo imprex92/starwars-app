ea2b043bf73b149a68e8efeb357fdfc8eb2caa45
Daniel Johannesson <daniel@johannesson.cz>
2020-05-12 02:22:10 +0200

    people, planet tabs + working paginate

--- src/components/ShowResult.jsx
+++ src/components/ShowResult.jsx
@@ -8,9 +8,11 @@ import './CSSfolder/Loader.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
-const ShowResult = ({peopleResults, planetResults, loading}) => {
+const ShowResult = ({peopleResults, planetResults, paginatePlanets, paginatePeoples, loading}) => {
 	
 	const [favorite, setFavorite] = useState(null)
+	const [toShow, setToShow] = useState('Peoples')
+
 	const handleFavorite = (person) => {
 		person.isFavorite = true;
 		console.log(person);
@@ -18,36 +20,54 @@ const ShowResult = ({peopleResults, planetResults, loading}) => {
 		db.collection('favResults').add(person)
 		
 	}
+	const handlePlanetFavorite = (planet) => {
+		planet.isFavorite = true;
+		console.log(planet);
+		const db = firebase.firestore()
+		db.collection('favPlanetResults').add(planet)
+		
+	}
+	
 	
 	if(loading){
-		return 	<div class="loader">
-					<div class="loader-wheel"></div>
-					<div class="loader-text"></div>
+		return 	<div className="loader">
+					<div className="loader-wheel"></div>
+					<div className="loader-text"></div>
 				</div>
 	}
 	return (
 		<div>
 			<Header/>
 			<main>
+				{/* Search Area */}
 				<div className="SearchArea mt-3 mb-4">
 					<h3>Search here:</h3>
 					<form >
-						<input type="text"/>
-						<button type="submit">Search</button>
+						<input className="rounded-pill" type="text"/>
+						<button className="rounded ml-2" type="submit">Search</button>
 					</form>
 				</div>
-				<ul className="list-group mb-4">
+					{/* List nav area */}
+				<nav className="list-nav ml-4">
+					<button onClick={() => {setToShow('Peoples'); paginatePeoples('Peoples')}} className="border-bottom-0">Peoples</button>
+					<button onClick={() => {setToShow('Planets'); paginatePlanets('Planets')}} className="border-bottom-0">Planets</button>
+				</nav>
+					{/* Peoples list area */}
+				<ul className='list-group mb-4' style={toShow === 'Peoples' ? {display: 'block'} : {display: 'none'}}>
 					
 					{peopleResults.map(person => (
 						
-					<li className="list-group-item ml-4" key={ person.url }>{ person.name }<span><FontAwesomeIcon style={favorite === person.name ? {color: 'yellow'} : {color: ''}} onClick={() => {setFavorite(person.name); handleFavorite(person);}} icon={ faStar } /></span></li>
+					<li className="list-group-item ml-4" key={ person.id }>{ person.name }<span className="star-span" ><FontAwesomeIcon style={favorite === person.name ? {color: 'yellow'} : {color: ''}} onClick={() => {setFavorite(person.name); handlePlanetFavorite(person);}} icon={ faStar } /></span></li>
 					))}
 				</ul>
-				{/* <div className="planetSection">
-					{ planetResults.map( planet => (
-						<div key={ planet.url }> { planet.name }</div>
+						{/* planet list area */}
+				<ul className='list-group mb-4' style={toShow === 'Planets' ? {display: 'block'} : {display: 'none'}}>
+					
+					{planetResults.map(planet => (
+						
+					<li className="list-group-item ml-4" key={ planet.id }>{ planet.name }<span className="star-span" ><FontAwesomeIcon style={favorite === planet.name ? {color: 'yellow'} : {color: ''}} onClick={() => {setFavorite(planet.name); handleFavorite(planet);}} icon={ faStar } /></span></li>
 					))}
-				</div> */}
+				</ul>
 			</main>
 			<Footer/>
 		</div>
