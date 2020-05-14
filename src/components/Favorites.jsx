b0df12b8d7d0a96f5be2336bb1f20f2a357f1c09
Daniel Johannesson <daniel@johannesson.cz>
2020-05-15 00:48:15 +0200

    datestamp to created,msg, add fav, preventDefault

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -2,18 +2,20 @@ import React, {useState, useEffect} from 'react'
 import firebase from './firebase/Firebase'
 import './CSSfolder/Favorites.css'
 import Header from './header/Header'
-import Footer from './footer/Footer'
-// import ShowFavResults from './ShowFavResults'
 import {FavoriteInput} from './FavoriteInput'
 
 function Favorites() {
 
 	const [loading, setLoading] = useState(false)
 	const [favResults, setFavResults] = useState([]) //TODO använd "setLocalStorage" custom hook (behövs ej längre)
-	const [newFavoriteName, setNewFavoriteName] = useState()
-	const [newFavoriteYear, setnewFavoriteYear] = useState()
-	const [newFavoritePlanet, setnewFavoritePlanet] = useState()
-	const [brodcastMsg, setBrodcastMsg] = useState(null) 
+	const [newFavoriteName, setNewFavoriteName] = useState('')
+	const [newFavoriteYear, setnewFavoriteYear] = useState('')
+	const [newFavoritePlanet, setnewFavoritePlanet] = useState('')
+	const [brodcastMsg, setBrodcastMsg] = useState(null)
+	const [isOK, setIsOK] = useState(null)
+	const [showSearch, setShowSearch] = useState(false)
+	const [typeOption, setTypeOption] = useState('person')
+	var date = new Date( )
 	useEffect(() => {
 		
 			setLoading(true)
@@ -24,10 +26,6 @@ function Favorites() {
 				snapshot.forEach(doc => favoriteData.push(({...doc.data(), id: doc.id})))
 				setFavResults(favoriteData)
 			})
-
-			// const favData = await db.collection('favResults').get()
-			// setFavResults(favData.docs.map(doc => ({...doc.data(), id: doc.id})))
-			
 			setLoading(false)
 			
 		return unsubscribe //? don't forget to unsubscribe when you use .onSnapshot()
@@ -36,39 +34,36 @@ function Favorites() {
 	
 
 	//TODO Validering krävs så användaren inte kan trycka på knappen om inte fälten är ifyllda!
-	const onCreate = () => {
+	const onCreate = (e) => {
 		const db = firebase.firestore()
 		db.collection('favResults').add({
+			created: date,
 			isCustom: true,
 			isFavorite: true,
+			ofType: typeOption,
 			name: newFavoriteName,
 			birth_year: newFavoriteYear,
-			homeworld: newFavoritePlanet
+			homeworld: newFavoritePlanet,
 		})
 		.then(function(docRef) {
 			console.log("Document written with ID: ", docRef.id);
 			setBrodcastMsg('Successfully added!')
-			
+			setIsOK(true)
 		})
 		.catch(function(error) {
-			console.error("Error adding document: ", error);
+			console.log("Error adding document: ", error);
 			setBrodcastMsg('Error! You messed up something! Try again.')
+			setIsOK(false)
 		});
+		e.preventDefault();
 	}
 
 	return (
-		<div>
+		<body>
 			<Header/>
-				<main>
-					<h2>Your Favorites</h2>
-					<form>
-						{/*//! onChange tar värdet som finns i inputfältet*/}
-						<input type="text" placeholder="Enter Name" value={newFavoriteName} onChange={(e) => setNewFavoriteName(e.target.value)}/> 
-						<input type="text" placeholder="Enter year of birth" value={newFavoriteYear} onChange={(e) => setnewFavoriteYear(e.target.value)}/>
-						<input type="text" placeholder="Enter HomePlanet" value={newFavoritePlanet} onChange={(e) => setnewFavoritePlanet(e.target.value)}/>
-					</form>
-						<button disabled={!newFavoriteName || !newFavoriteYear || !newFavoritePlanet} onClick={onCreate}>Save new favorite</button> <span className="onSaveFav">{brodcastMsg}</span>
 				
+					<h2>Your Favorites</h2>
+					
 					{/* <div className="peopleSection">
 						{peopleResults.map(person => (
 							<div key={ person.url }>{ person.name }</div>
@@ -99,12 +94,30 @@ function Favorites() {
 							<ShowFavResults favResults={favResults} loading={loading}>
 							
 							</ShowFavResults>
-						</div> */}
-
-				</main>
-			<Footer/>
-		</div>
+						</div> */}																						
+						<span className="add-favorite-section" onClick={() => setShowSearch(!showSearch)}>Add a Favorit</span>
+					<div className='add-favorite-dropdown' style={showSearch ? {display: 'block'} : {display: 'none'}}>
+						<form className="form-text-input">
+							{/*//! onChange tar värdet som finns i inputfältet*/}
+							<input type="text" placeholder="Enter Name" value={newFavoriteName} onChange={(e) => setNewFavoriteName(e.target.value)}/> 
+							<input type="text" placeholder="Enter year of birth" value={newFavoriteYear} onChange={(e) => setnewFavoriteYear(e.target.value)}/>
+							<input type="text" placeholder="Enter HomePlanet" value={newFavoritePlanet} onChange={(e) => setnewFavoritePlanet(e.target.value)}/> <br/>
+							<p>Is this a person or planet you're adding?</p>
+								<div className="form-favorite-radio">
+									<label htmlFor="person-radio">A Person</label>
+									<label htmlFor="planet-radio">A Planet</label>
+									<input onChange={(e) => setTypeOption(e.target.value)} defaultChecked={true} type="radio" name="ofType" id="person-radio" value="person"/>
+									<input onChange={(e) => setTypeOption(e.target.value)} type="radio" name="ofType" id="planet-radio" value="planet"/>
+								</div>
+								
+							<button type="submit" value="Submit" disabled={!newFavoriteName || !newFavoriteYear || !newFavoritePlanet} onClick={(e) => onCreate(e)}>Save new favorite</button> <br/>
+							<span className={isOK ? 'brodcast-OK' : 'brodcast-error'}> { brodcastMsg } </span>
+						</form>
+					</div>
+				
+		</body>
 	)
 }
+//TODO filter/map favResult depending on isPerson pr isPlanet in different tabs
 
 export default Favorites
