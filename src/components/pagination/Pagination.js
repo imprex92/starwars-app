a81261012e878835a0b2b53a3a586460e44d942d
Daniel Johannesson <daniel@johannesson.cz>
2020-05-15 00:44:03 +0200

    minor CSS changes

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 //! tar in tre (3) PROPS
 const Pagination = ({ postPerPage, totalPosts, paginate }) => {
 	const pageNumbers = [];
+	const [active, setActive] = useState(1)
 
 	//! Nedan räknar ut totalt antal sidor som finns
 	for(let i = 1; i <= Math.ceil(totalPosts / postPerPage); i++){
@@ -10,17 +11,18 @@ const Pagination = ({ postPerPage, totalPosts, paginate }) => {
 	}
 		//! Här är nav för alla våra sidor.
 	return (
-		<nav>
-			<ul className="pagination ml-4" >
+
+		<div>
+			<ul className="pagination justify-content-center" >
 				{pageNumbers.map(number => (
-					<li key={number} className="page-item">
-						<span onClick={() => paginate(number)} href="" className="page-link"> {/*onClick anropar metoden paginate som vi tilldelar number-variabeln paginate blir anropad som en prop */}
+					<li key={number} className={number === active ? 'page-item active' : 'page-item'}>
+						<span onClick={() => {paginate(number); setActive(number)}} href="" className="page-link"> {/*onClick anropar metoden paginate som vi tilldelar number-variabeln paginate blir anropad som en prop */}
 							{number}
 						</span>
 					</li>
 				))}
 			</ul>
-		</nav>
+		</div>
 	)
 }
 
