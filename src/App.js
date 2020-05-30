afe378c84ddf2b54493f01d0d24cad472c6390e1
Daniel Johannesson <daniel@johannesson.cz>
2020-05-30 23:41:26 +0200

    removed footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
+
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
 			<Header/>
-				<body>
-					This is the Home Page
-				</body>
-			<Footer/>
+			
+		
+			
 		</div>
 	);
 }
 
 export default App;
+
+
+
+//TODO skriv it custom 
